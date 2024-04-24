be1667989c0d8ddd3c32be334050442a47446e54
Nikita <nickit.mihajlov2015@yandex.ru>
2024-04-25 01:46:11 +0300

    123

--- Python_task/Task_3/task_3.py
+++ Python_task/Task_3/task_3.py
@@ -1,18 +1,19 @@
-a = int(input())
-b = []
-c = -1000
-if a <= c or a >= abs(c):
-    print(a, "- четырехзначное число")
-a = str(a)
-for i in a:
-    b.append(i)
-if b[0] == "-":
-    b[0] + b[1]
-if b[0] == b[1] or b[1] == b[2] or b[2] == b[0]:
-    print("в числе есть одинаковые цифры")
-print(b[0], b[1], b[2])
-print(b[0], b[2], b[1])
-print(b[1], b[0], b[2])
-print(b[1], b[2], b[0])
-print(b[2], b[1], b[0])
-print(b[2], b[0], b[1])
+from itertools import permutations
+def main():
+    number = int(input())
+    List = []
+    c = -1000
+    if number <= c or number >= abs(c):
+        print(number, "- четырехзначное число")
+        return
+    number = str(number)
+    for i in number:
+        List.append(i)
+    if List[0] == List[1] or List[1] == List[2] or List[2] == List[0]:
+        print("В числе есть одинаковые цифры")
+    permutations_list = list(permutations(List))
+    for perm in permutations_list:
+        for i in perm:
+            print(i, end=" ")
+        print()
+main()
