4636f92248a1ed8cb9c2c6ae00e065c8edcd6d75
Nikita <nickit.mihajlov2015@yandex.ru>
2024-05-05 15:49:03 +0300

    2 вариант

--- Python_task5/Task1_2v/Task1.py
+++ Python_task5/Task1_2v/Task1.py
@@ -1,4 +1,7 @@
 import random
+import re
+import calendar
+from datetime import date
 
 list_el = [100, 400, 500, 10, 50, "Банан", "Клубника", "Камень", "Морковь", "Огурец", "Пицца"]
 
@@ -11,3 +14,33 @@ def casino(list_el):
 
 
 print(casino(list_el))
+calendar_date = str(input("Введите дату формата «ГГГГ-ММ»: "))
+
+
+def show_calendar(calendar_date):
+    pattern = re.compile(r'\d{4}-\d{2}')
+
+    if pattern.match(calendar_date):
+        year, month = map(int, calendar_date.split('-'))
+        cal = calendar.monthcalendar(year, month)
+        for week in cal:
+            print(week)
+    else:
+        print("Неверный формат даты. Пожалуйста, введите дату в формате 'ГГГГ-ММ'.")
+
+
+show_calendar(calendar_date)
+
+phone_number = str(input("Введите российский номер телефона формата '+7-***-***-**-**': "))
+
+
+def phone_number_check(phone_number):
+    pattern = r'^(\+7|7|8)-\d{3}-\d{3}-\d{2}-\d{2}$'
+
+    if re.match(pattern, phone_number):
+        print("Номер телефона {} соответствует формату".format(phone_number))
+    else:
+        print("Номер телефона {} не соответствует формату".format(phone_number))
+
+
+phone_number_check(phone_number)
